96487ac322d630d72da4a37605c02ac0cbe187c8
7vTr0x7 <7vTrox7@gmail.com>
2024-05-21 15:13:55 +0530

    ES1.1_CW

--- ES.1.1_CW/index.js
+++ ES.1.1_CW/index.js
@@ -1,2 +1,45 @@
-let numericNumber = 7;
-console.log(numericNumber);
+let NumericVariable = 7;
+console.log(NumericVariable);
+
+let stringVariable = "Hello";
+console.log(stringVariable);
+
+let firstName = "not";
+let lastName = "Found";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+let numberVariable = 10;
+numberVariable = 20;
+console.log(numberVariable);
+
+let num1 = 30;
+let num2 = 20;
+let sum = num1 + num2;
+let diff = num1 - num2;
+
+console.log("Sum: ", sum);
+console.log("Diff: ", diff);
+
+let newName;
+newName = "non";
+console.log(newName);
+
+const numericConst = 20;
+console.log(numericConst);
+
+const stringConst = "hii";
+console.log(stringConst);
+
+const arr = [1, 2, 3, 45];
+
+arr[0] = 5;
+console.log(arr);
+
+const obj = {
+  name: "non",
+  age: 22,
+};
+
+obj.age = 21;
+console.log(obj);
