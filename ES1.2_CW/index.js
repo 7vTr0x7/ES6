b19a12b293058733bf9a8b6ac029c14854d3fc5a
7vTr0x7 <7vTrox7@gmail.com>
2024-05-21 16:01:38 +0530

    ES1.2_CW

---
+++ ES1.2_CW/index.js
@@ -0,0 +1,59 @@
+// let a = 2;
+
+// if (true) {
+//   console.log(a);
+// }
+
+// console.log(a);
+
+// const a = 2;
+
+// if (true) {
+//   console.log(a);
+// }
+
+// console.log(a);
+
+// let num = 2;
+
+// if (true) {
+//   let num = 200;
+// }
+
+// console.log(num);
+
+// let num = 20;
+
+// function getNum() {
+//   const num = 2000;
+//   console.log(num);
+
+//   if (true) {
+//     const num = 22;
+//     console.log(num);
+//   }
+
+//   console.log(num);
+// }
+
+// getNum();
+
+// console.log(num);
+
+// let num1 = 20;
+
+// if (true) {
+//   num1 = 22;
+//   console.log(num1);
+// }
+// console.log(num1);
+
+// let userName;
+// console.log(userName);
+// userName ="non"
+
+// console.log(userName);
+// const userName = "non";
+
+console.log(userName);
+var userName = "non";
