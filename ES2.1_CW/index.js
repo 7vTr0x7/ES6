f0ff5b8991a6ff61b11af16aa6e500f81a84a7bc
7vTr0x7 <7vTrox7@gmail.com>
2024-05-22 16:42:08 +0530

    ES2.1_CW

---
+++ ES2.1_CW/index.js
@@ -0,0 +1,22 @@
+// //Tradition way
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// console.log(add(4, 5));
+
+// ES6 Way- Arrow Function
+
+const add = (a, b) => a + b;
+console.log(add(4, 5));
+
+//multiply two numbers
+
+const multi = (a, b) => a * b;
+console.log(multi(4, 5));
+
+//concatenate two strings
+
+const concatString = (a, b) => a + " " + b;
+console.log(concatString("Hello", "World"));
